ec8665b7c6965d5c48a03da5781e31680515ede1
Vincent Roscioli <git@veearr.dev>
2026-08-11 18:03:42 -0400

    Future classes

--- relay/laughingstonks_dump.js
+++ relay/laughingstonks_dump.js
@@ -19,18 +19,17 @@ function handleDump(){
 	
 	let toc="<h2><a href='https://github.com/VeeArrKoL/laughingstonks'>laughingstonks</a></h2><a name='toc'/><table><tr style='background-color:#FFFFFF; vertical-align:top'><td><table>\n";
 	let tables="";
-	let idx=0;
 	let i=0;
 	let combos=getClassPathCombos();
 	for(pathData of combos){
 		toc+="<tr><td>"+pathData.pathName+"</td>\n";
 		let w=0;
 		for(clazz of pathData.classes){
-			idx++;
 			w++;
-			let abbrClassName=clazz.className.match(/\b\S/g).join("").toUpperCase();
-			toc+="<td><a href='#a"+idx+"'>"+abbrClassName+"</a></td>";
-			tables+="<h3><a name='a"+idx+"'/><b>"+pathData.pathName+" / "+clazz.className+"</b> <a style='text-decoration:none' href='#toc'>&#10548;</a></h3>";
+			let abbrClassName=clazz.className.match(/\b\S|\d/g).join("").toUpperCase();
+			let anchorName="p"+pathData.pathId+"_c"+clazz.classId;
+			toc+="<td><a href='#"+anchorName+"'>"+abbrClassName+"</a></td>";
+			tables+="<h3><a name='"+anchorName+"'/><b>"+pathData.pathName+" / "+clazz.className+"</b> <a style='text-decoration:none' href='#toc'>&#10548;</a></h3>";
 			tables+=LSJ.buildTable(clazz.classId,pathData.pathId,1,4);
 		}
 		if(w<6){
@@ -51,16 +50,23 @@ function handleDump(){
 function getClassPathCombos(){
 	rv=[];
 	
-	let nextId=Path.all().sort((a,b)=>b.id-a.id)[1].id+1;
+	let nextPId=Path.all().sort((a,b)=>b.id-a.id)[1].id+1;
+	let nextPath={id:nextPId,name:"Path #"+nextPId,forceBasics:true};
+	let nextCId=Class.all().sort((a,b)=>b.id-a.id)[0].id+1;
+	let nextClasses=[];
+	for(i=0;i<3;i++){
+		nextClasses.push({id:nextCId+i,name:"Class #"+(nextCId+i),path:{id:nextPId}});
+	}
 				
-	for(path of [{id:0,name:"Unrestricted"},{id:nextId,name:"&lt;next path: ID #"+nextId+"&gt;"}, ...Path.all()]){
+	for(path of [{id:0,name:"Unrestricted"},nextPath,...Path.all()]){
 		let pathData={pathName:path.name,pathId:path.id,classes:[]};
-		for(clazz of Class.all()){
+		for(clazz of [...nextClasses,...Class.all()]){
 			if(clazz.path.id==path.id){
-				pathData.classes.push({classId:clazz.id,className:kol.toString(clazz)});
+				let className=clazz.name??kol.toString(clazz);
+				pathData.classes.push({classId:clazz.id,className});
 			}
 		}
-		if(pathData.classes.length==0){
+		if(pathData.classes.length==0||path.forceBasics){
 			for(i=1;i<=6;i++){
 				clazz=kol.toClass(i);
 				pathData.classes.push({classId:clazz.id,className:kol.toString(clazz)});
